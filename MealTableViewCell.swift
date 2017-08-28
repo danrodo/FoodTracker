ba60735ae9b1c2162eaa3a1948fed6308f9d6576
Daniel Rodosky <djrodosky@gmail.com>
2017-08-28 17:27:50 -0600

    Initial commit

---
+++ MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Daniel Rodosky on 8/28/17.
+//  Copyright © 2017 Dan Rodosky. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var mealLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
